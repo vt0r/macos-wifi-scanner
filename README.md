a7a8e8b59737ba6da9e0c6926b759375525a5086
Salvatore LaMendola <salvatore@lamendo.la>
2025-12-17 22:22:12 -0500

    Add macOS 26 support
    - Also make the script executable and update README

--- README.md
+++ README.md
@@ -1,42 +1,47 @@
 # Mac OS Wifi Scanner
 
-There is no `iwlist` on Mac OS, that is why I've created my own script to get all SSID around me via cli ;) 
+There is no `iwlist` on Mac OS, that is why I've created my own script to get all SSID around me via cli ;)
 
-
-## Prerequisites:
+## Prerequisites
 
 Before running anything, check out the `requirements.txt` and make sure you have everything neccessary to run this script installed. To install all needed requirements:
 
 ```bash
-pip install -r requirements.txt
+# Create a Python virtualenv
+python3 -m venv .venv
+
+# Source the active script to use venv
+source .venv/bin/activate
+
+# Install module requirements
+pip3 install -r requirements.txt
 ```
 
-Then run a script:
+## Running the script
 
 ```bash
 
-python3 wifi-scan
+./wifi_scan.py
 
 ```
 
+### Sample output
 
-## Example:
-
-How to use it:
-
-```bash
-python3 wifi-scan.py
-
+```python
 {'SOME WIFI': {'BSSID': '11:11:11:11:11:11', 'RSSI': -45},
  'ANOTHER_WIFI': {'BSSID': '11:11:11:11:11:11', 'RSSI': -57}}
-
 ```
 
-Wifi-scan.py support `filter` argument, which can be used to find any particular SSID.
+### Further usage info
+
+`wifi_scan.py` supports a `--filter` (`-f`) argument, which can be used to find any particular SSID.
 
 ```bash
-python3 wifi-scan.py -f SOME_WIFI
+./wifi_scan.py -f SOME_WIFI_SSID
+```
 
+...which would result in similar output to the folowing:
+
+``` python
 {'SOME WIFI': {'BSSID': '11:11:11:11:11:11', 'RSSI': -45}
-
 ```
---
+++ wifi_scan.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+wifi_scan.py - a script to dump the list of available SSIDs and basic info
+"""
+import argparse
+from pprint import pprint
+import objc
+import CoreLocation
+
+parse = argparse.ArgumentParser("Hello, I'm a Wi-Fi scanner for macOS\n\n" +
+                                "Please allow the location request for python if prompted.")
+parse.add_argument("--filter", '-f', help="Single SSID to filter on", default=None)
+
+args = parse.parse_args()
+filter_ssid = args.filter
+
+# Request location access, so that we can view the SSIDs
+location_manager = CoreLocation.CLLocationManager.alloc().init()
+location_manager.startUpdatingLocation()
+
+
+def scan(concrete_ssid=None):
+    """
+    Loads the CoreWLAN bundle, instantiates an interface(),
+    dumps the list of available networks, including ones with hidden SSIDs,
+    then prints the SSID, RSSI, BSSID for each result
+
+    Optional argument:
+      concrete_ssid - the SSID of one single network to filter on
+    """
+    bundle_path = '/System/Library/Frameworks/CoreWLAN.framework'
+    objc.loadBundle('CoreWLAN',
+                    bundle_path=bundle_path,
+                    module_globals=globals())
+
+    iface = CWInterface.interface()
+    networks = iface.scanForNetworksWithName_includeHidden_error_(concrete_ssid, True, None)
+    return {
+        i.ssid(): {
+            'RSSI': i.rssiValue(),
+            'BSSID': i.bssid()
+        }
+        for i in networks[0].allObjects() if i.ssid() is not None
+    }
+
+
+result = scan(filter_ssid)
+
+if result is None:
+    print("Sorry, couldn't find that SSID")
+
+pprint(result)
